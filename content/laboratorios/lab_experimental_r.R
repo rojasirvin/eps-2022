dacfd448058d9e760bea44e17c4ab4ca33613a19
rojasirvin <rojasirvin@gmail.com>
2022-09-15 07:56:07 -0500

    fix lab 1

--- content/laboratorios/lab_experimental_r.R
+++ content/laboratorios/lab_experimental_r.R
@@ -15,7 +15,7 @@ library(stargazer)
 #a los reportados en el artículo
 
 data.angrist <- read_csv("./content/laboratorios/STAR_public_use.csv",
-                       locale = locale(encoding = "latin1"))   %>% 
+                         locale = locale(encoding = "latin1"))   %>% 
   clean_names()
 
 
@@ -24,10 +24,16 @@ data.angrist.table1 <- data.angrist %>%
   select(noshow,age,female, mtongue, gpa0, sfsp) %>%
   mutate(mtongue=ifelse(mtongue=="English",1,0)) %>% 
   mutate(sfsp=factor(sfsp,levels=c(0,1),
-             labels=c("Control","SFSP")))
+                     labels=c("Control","SFSP")))
 
 #Pedimos estadísticas por grupo
-[FALTA]
+data.angrist.table1 %>% 
+  filter(noshow==0) %>% 
+  group_by(sfsp) %>% 
+  summarize(mean=mean(age),
+            std=sd(age), n=n()) %>% 
+  ungroup()
+
 
 #Prueba t
 t.test(data=filter(data.angrist.table1,noshow==0),age ~ sfsp)
@@ -37,18 +43,23 @@ dif_age <- lm(age ~ sfsp, data=filter(data.angrist.table1,noshow==0))
 summary(dif_age)
 
 #¿Las x predicen tratamiento?
-[FALTA]
+predict_t <- lm(as.numeric(sfsp) ~ age + female + mtongue + gpa0, data=filter(data.angrist.table1,noshow==0) )
 summary(predict_t)
 
 
 #Una forma muy rápida con modelsummary ----
 datasummary(noshow+age+female~ sfsp*(mean + sd)*Arguments(na.rm=TRUE),
+            fmt = "%.2f",
+            data = data.angrist.table1,
+            title = "Pruebas de balance",
+            notes = "Fuente: Angrist, Lang & Oreopoulos (2009)")
+
+datasummary_balance(~sfsp,
                     fmt = "%.2f",
                     data = data.angrist.table1,
                     title = "Pruebas de balance",
                     notes = "Fuente: Angrist, Lang & Oreopoulos (2009)")
 
-[FALTA]
 
 
 
@@ -60,7 +71,7 @@ reg <- lm(gpa_year1 ~ sfsp +
             factor(hsgroup)+
             factor(mom_edn)+
             factor(dad_edn),
-            data=filter(data.angrist, noshow==0))
+          data=filter(data.angrist, noshow==0))
 
 summary(reg)$coef[1:2,]
 
@@ -68,7 +79,12 @@ summary(reg)$coef[1:2,]
 
 
 #Uso stargazer para presentar resultados (hay otras maneras)
-[FALTA]
+stargazer(reg,
+          type = "text",
+          se = list(NULL),
+          column.labels = "Clásicos",
+          keep = c("ssp", "sfp", "sfsp"))
+
 
 #Comparamos errores con robustos
 cov0       <- vcovHC(reg, type = "HC0")
@@ -84,7 +100,9 @@ stargazer(reg, reg,
 
 
 #Ahora errores HC3 (corrección por influencia), el default
-[FALTA]
+
+cov3      <- vcovHC(reg)
+robse3    <- sqrt(diag(cov3))
 
 stargazer(reg, reg, reg,
           type = "text",
