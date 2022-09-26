d9095063b8af7b1d88b03371ec502132bba8cec7
rojasirvin <rojasirvin@gmail.com>
2022-09-26 17:50:47 -0500

    fix laboratorios

--- content/laboratorios/lab_multiples_hipotesis.R
+++ content/laboratorios/lab_multiples_hipotesis.R
@@ -5,20 +5,19 @@ options(scipen=999) # Prevenir notación científica
 library(tidyverse)
 
 #Datos
-data.pvalues<-read_csv("./data_benjamini_hochberg.csv",
+data.pvalues<-read_csv("./content/laboratorios/data_benjamini_hochberg.csv",
                        locale = locale(encoding = "latin1"))  
-n <- 15
+m <- 15
 alpha <- 0.05
 
 data.pvalues
 
-
 #Bonferroni----
 data.bonferroni <- data.pvalues %>% 
-  mutate(bonferroni_alpha=alpha/n) %>% 
+  mutate(bonferroni_alpha=alpha/m) %>% 
   mutate(bonferrini_rechazar=ifelse(poriginal<=bonferroni_alpha,1,0))
 
 #Benjamini-Hochberg----
 data.bh <- data.pvalues %>% 
-  mutate(bh_alpha=alpha*hipotesis/n) %>% 
+  mutate(bh_alpha=alpha*hipotesis/m) %>% 
   mutate(bh_rechazar=ifelse(poriginal<=bh_alpha,1,0))
